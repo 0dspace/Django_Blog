efbd28b7d3dfa75e93b40c3effd223423ecd0af9
dspace <zzgdata@gmail.com>
2018-04-14 00:23:38 +0800

    学会了路由的配置以及模板的使用

---
+++ articles/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Article
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render_to_response('articles.html', {'articles': articles})
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render_to_response('article_detail.html', {'article': article})
+
+def article_detail2(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+        # return render(request, 'article_detail.html', {'article': article})
+        return render_to_response('article_detail.html', {'article': article})
+    except Article.DoesNotExist:
+        raise Http404('此页面不存在或已丢失.')
+    # return HttpResponse('Title: {0}'.format(article.title))
